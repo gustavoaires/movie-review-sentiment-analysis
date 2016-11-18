87a024b60fd7fd53e6a121819410b25734bd8c74
gustavoaires <gustavoairesmatos@gmail.com>
2016-11-18 16:02:39 -0300

    nltk working

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 stopset = set(stopwords.words('english'))
 
-with open('sentiment_test.txt', 'r') as text_file:
+with open('/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/neg/cv000_29416.txt', 'r') as text_file:
     text = text_file.read()
-    tokens=word_tokenize(str(text))
-    tokens = [w for w in tokens if not w in stopset]
+    tokens = word_tokenize(str(text))
+    tokens = [w for w in tokens if w not in stopset]
     print tokens
