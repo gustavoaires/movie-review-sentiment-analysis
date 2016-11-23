9aee0b9e4c985c0f9b26a29b40e3a945e924d27c
gustavoaires <gustavoairesmatos@gmail.com>
2016-11-23 17:21:02 -0300

    Iniciando labels dos dados

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
 import nltk
+import os
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 stopset = set(stopwords.words('english'))
+bad_symbols = ["\"", "\'", "!", "?", ".", "," , ";", ">", "_", "<", "-", "[", "]", "{", "}", "'s", "\/", "\\", "^", "~", "'", "`", "``", ":", "(", ")", "@", "#", "$", "%", "&"]
 
-with open('/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/neg/cv000_29416.txt', 'r') as text_file:
-    text = text_file.read()
-    tokens = word_tokenize(str(text))
-    tokens = [w for w in tokens if w not in stopset]
-    print tokens
+files_pos = []
+files_neg = []
+
+#fill files_neg
+files_names = os.listdir("/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/neg")
+files_neg = files_names[0:299]
+
+#fill files_pos
+files_names = os.listdir("/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/pos")
+files_pos = files_names[0:299]
+
+tokens_with_labels = [][]
+
+def read_file(path, label):
+	with open(path, 'r') as text_file:
+		text = text_file.read()
+		tokens = str(text).split()
+		tokens = [w for w in tokens if w not in stopset]
+		tokens = [w for w in tokens if w not in bad_symbols]
+		tokens_with_labels.append(tokens, label)
+		
+for i in range(0, 300):
+	path = "/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/neg/", files_neg[i]
+	read_file(path, 'neg')
+	
+		
+print tokens_with_labels
