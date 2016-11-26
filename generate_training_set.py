c419e3a9d5da6d795e4ae16f1c18ee7e0f62d2f0
gustavoaires <gustavoairesmatos@gmail.com>
2016-11-26 20:21:39 -0300

    stemming training set complete

--- generate_training_set.py
+++ generate_training_set.py
@@ -1,57 +1,71 @@
 import nltk
 import os
 import csv
+import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 
-stopset = set(stopwords.words('english'))
+stemmer = SnowballStemmer("english")
+stopset = set(stopwords.words("english"))
 symbols = ["\"", "\'", "!", "?", ".", "," , ";", ">", "_", "<", "-", "[",
 			"]", "{", "}", "'s", "\/", "\\", "^", "~", "'", "`", "``",
 			":", "(", ")", "@", "#", "$", "%", "&", "*", "=", "+"]
 
-files_pos = []
-files_neg = []
+pos_files = []
+neg_files = []
 
 incomplete_path = "/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/"
 
-#fill files_neg
-files_names = os.listdir(incomplete_path + "/neg")
-files_neg = files_names[0:700]
+#loading neg_files
+file_names = os.listdir(incomplete_path + "neg")
+neg_files = file_names[0:700]
 
-#fill files_pos
-files_names = os.listdir(incomplete_path + "/pos")
-files_pos = files_names[0:700]
+#loading pos_files
+file_names = os.listdir(incomplete_path + "pos")
+pos_files = file_names[0:700]
 
-tokens_with_labels = []
+training_set = []
+
+#path to save training set
+training_set_path = '/home/gustavo/git/sentiment_analysis/training_set.csv'
 
 #reads the file
 def read_file(path, label):
 	with open(path, 'r') as text_file:
 		text = text_file.read()
 		tokens = str(text).split()
-		tokens = [w for w in tokens if w not in stopset]
-		tokens = [w for w in tokens if w not in symbols]
-		tokens_with_labels.append((tokens, label))
+		valid_tokens = [w for w in tokens if (w not in stopset) and (w not in symbols) and (re.search('[a-zA-Z]', w))]
+		tokens = []
+		for token in valid_tokens:
+			tokens.append(str((stemmer.stem(token))))
+		training_set.append((tokens, label))
 		
 #iterate all file names to read and create tuples
-for i in range(0, 700):
-	path = incomplete_path + "neg/" + files_neg[i]
-	read_file(path, 'neg')
-	path = incomplete_path + "pos/" + files_pos[i]
-	read_file(path, 'pos')
-
-tokens_labels = '/home/gustavo/git/sentiment_analysis/tokens_labels.csv'
+def get_file_name():
+	for i in range(0, 700):
+		path = incomplete_path + "neg/" + neg_files[i]
+		read_file(path, 'neg')
+		path = incomplete_path + "pos/" + pos_files[i]
+		read_file(path, 'pos')
 	
 #save data (tokens with labels) into file
-with open(tokens_labels, 'wb') as csvfile:
-	writer = csv.writer(csvfile)
-	for row in tokens_with_labels:
-		data = []
-		for w in row[0]:
-			data.append(w)
-		data.append(row[1])
-		writer.writerow(data)
-print tokens_with_labels
+def write_training_set():
+	with open(training_set_path, 'wb') as csvfile:
+		writer = csv.writer(csvfile)
+		for row in training_set:
+			data = []
+			for w in row[0]:
+				data.append(w)
+			data.append(row[1])
+			writer.writerow(data)
+
+def main():
+	get_file_name()
+	write_training_set()
+	print training_set
+
+main()
 
 """	
 	code to read file
@@ -60,4 +74,4 @@ print tokens_with_labels
 #with open(tokens_labels, 'rb') as csvfile:
 #	spamreader = csv.reader(csvfile)
 #	for row in spamreader:
-#		<storage-tokens-matriz>.append((row[0:-1], row[-1]))
+#		<training_set>.append((row[0:-1], row[-1]))
