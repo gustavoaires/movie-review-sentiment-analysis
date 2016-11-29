8553ce2238041f788aaaba0ebd7194c0ce9f4564
gustavoaires <gustavoairesmatos@gmail.com>
2016-11-29 02:39:27 -0300

    Adding classifier algorithm. Needs to fix a bug in line 171

--- classifier.py
+++ classifier.py
@@ -6,7 +6,6 @@ import re
 import pandas
 import operator
 import math
-
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
@@ -26,17 +25,19 @@ incomplete_path = "/home/gustavo/git/sentiment_analysis/review_polarity/txt_sent
 #loading neg_files
 file_names = os.listdir(incomplete_path + "neg")
 neg_files = file_names[0:700]
-neg_files_test_set = file_names[701:1000]
+neg_files_test_set = file_names[700:1000]
 
 #loading pos_files
 file_names = os.listdir(incomplete_path + "pos")
 pos_files = file_names[0:700]
-pos_files_test_set = file_names[701:1000]
+pos_files_test_set = file_names[700:1000]
 
 training_set = []
+test_set = []
 
 #path to save training set
 training_set_path = '/home/gustavo/git/sentiment_analysis/training_set.csv'
+test_set_path = '/home/gustavo/git/sentiment_analysis/test_set.csv'
 
 #reads the file
 def read_file(path, label):
@@ -67,7 +68,8 @@ def write_training_set():
 				data.append(w)
 			data.append(row[1])
 			writer.writerow(data)
-		
+
+#words to indexing data frame		
 words = Set()
 		
 #read training set file		
@@ -125,14 +127,74 @@ def calculate_class_probability(df):
 		total = total + 1.0
 	class_prob_neg = count_neg / total
 	class_prob_pos = count_pos / total
+	aux = 1 - (class_prob_neg + class_prob_pos)
+	class_prob_neg = class_prob_neg + aux / 2
+	class_prob_pos = class_prob_pos + aux / 2
 	return class_prob_neg, class_prob_pos
+
+def read_file_test_set(path, label):
+	with open(path, 'r') as text_file:
+		text = text_file.read()
+		tokens = str(text).split()
+		valid_tokens = [w for w in tokens if (w not in stopset) and (w not in symbols) and (re.search('[a-zA-Z]', w))]
+		tokens = []
+		for token in valid_tokens:
+			tokens.append(str(stemmer.stem(token)))
+		test_set.append(tokens)
+
+def write_test_set():
+	with open(test_set_path, 'wb') as csvfile:
+		writer = csv.writer(csvfile)
+		for row in test_set:
+			writer.writerow(row)		
+	
+def create_test_set():
+	for i in range(0, 300):
+		path = incomplete_path + "neg/" + neg_files_test_set[i]
+		read_file_test_set(path, 'neg')
+		path = incomplete_path + "pos/" + pos_files_test_set[i]
+		read_file_test_set(path, 'pos')
+	write_test_set()
+	
+def naive_bayes_classify(document, labels, processed_words, class_probabilities, words_per_class):
+	no_words_in_doc = len(document)
+	current_class_prob = {}
+	for label in labels:
+		prob = math.log(class_probabilities[label],2) - no_words_in_doc * math.log(words_per_class[label],2)
+		for word in document:
+			if word in processed_words:
+				occurence = df.loc[word][label]
+                if occurence > 0:
+                    prob = prob + math.log(occurence,2)
+                else:
+                    prob = prob + math.log(1,2)
+			else:
+                prob = prob + math.log(1,2)
+        current_class_prob[label] = prob
+	sorted_labels = sorted(current_class_prob.items(), key=operator.itemgetter(1))
+    most_probable_class = sorted_labels[-1][0]
+    return most_probable_class
 	
 def main():
+	#create_test_set()
 	#read_training_set()
 	#count_freq()
 	#write_data_frame()		
-	class_prob_neg, class_prob_pos = calculate_class_probability(read_data_frame())
-	print class_prob_neg, class_prob_pos
+	df = read_data_frame()
+	class_prob_neg, class_prob_pos = calculate_class_probability(df)
+	
+	processed_words = list(df.index.values)
+	class_probabilities = {'neg' : class_prob_neg, 'pos' : class_prob_pos}
+	labels = class_probabilities.keys()
+	words_per_class = {}
+	
+	for label in labels:
+		words_per_class[label] = df[label].sum()
+		
+	for document in test_set:
+		classification = naive_bayes_classify(document, labels, processed_words, class_probabilities, words_per_class)
+		row.append(classification)
+		print row
 
 main()
 
